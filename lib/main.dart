e81aab8764d38c0cdcf234d023e1d7016a620f2a
KevinATJ <kevintjara@gmail.com>
2024-09-04 18:50:18 -0400

    Correción de un warning

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_lab1/pages/myhomepage.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:logger/logger.dart'; 
 
